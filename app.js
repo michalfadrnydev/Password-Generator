3321d9a68071e5987bfc0e7b692e0ca9b5213d68
Michal Fadrný <120096517+michalfadrnydev@users.noreply.github.com>
2023-03-01 20:44:20 +0100

    Starter

---
+++ app.js
@@ -0,0 +1,5 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const symbols = ["+", "-", "&", "!", ":","_", "*", "?", ":", "#", "@"];
+
+let password_lenght = 5;
